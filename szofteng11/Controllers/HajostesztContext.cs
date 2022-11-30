0a18b91cb56858807561bedb69afb1aa4eaac6df
fsswjj <fsswjj@SZA0030883>
2022-11-30 11:10:13 +0100

    Add project files.

---
+++ szofteng11/Controllers/HajostesztContext.cs
@@ -0,0 +1,8 @@
+﻿namespace szofteng11.Controllers
+{
+    internal class HajostesztContext
+    {
+        public object Questions { get; internal set; }
+        public object Question { get; internal set; }
+    }
+}
